b2f16d2ad9c6637669921540b804656f44295a25
Petr Sapak <petr.sapak@yahoo.com>
2021-09-04 17:35:33 +0200

    Remove CSGORanks class

--- TeamGenerator.Core.Test/EvaluatorTestHelper.cs
+++ TeamGenerator.Core.Test/EvaluatorTestHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using TeamGenerator.Model;
 
 namespace TeamGenerator.Core.Tests
@@ -9,14 +10,34 @@ namespace TeamGenerator.Core.Tests
         {
             Random random = new Random();
             Team team = new Team("Test");
-            CSGORanks ranks = new CSGORanks();
+            List<Rank> ranks = new List<Rank>
+            {
+                new Rank("Silver 1", 1),
+                new Rank("Silver 2", 2),
+                new Rank("Silver 3", 3),
+                new Rank("Silver 4", 4),
+                new Rank("Silver Master", 5),
+                new Rank("Silver Master Elite", 6),
+                new Rank("Golden Nova 1", 7),
+                new Rank("Golden Nova 2", 8),
+                new Rank("Golden Nova 3", 9),
+                new Rank("Golden Nova Master", 10),
+                new Rank("Master Guardian 1", 11),
+                new Rank("Master Guardian 2", 12),
+                new Rank("Master Guardian Elite", 13),
+                new Rank("Distinguished Master Guardian", 14),
+                new Rank("Legendary Eagle", 15),
+                new Rank("Legendary Eagle Master", 16),
+                new Rank("Supreme Master First Class", 17),
+                new Rank("Global Elite", 18),
+            };
 
             int teamSize = random.Next(3, 9);
 
             for (int i = 0; i < teamSize; i++)
             {
                 string name = $"Player{i}";
-                Rank rank = ranks.Ranks[random.Next(ranks.Ranks.Count)];
+                Rank rank = ranks[random.Next(ranks.Count)];
                 Player player = new Player(name, rank);
 
                 team.AddPlayer(player);
