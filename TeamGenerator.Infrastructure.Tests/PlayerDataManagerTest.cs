198f897d203d31a697e01b9f3f08e755701af0c7
Petr Sapak <petr.sapak@yahoo.com>
2021-09-04 19:03:37 +0200

    Add serialization and deserialization for the player pool

--- TeamGenerator.Infrastructure.Tests/PlayerDataManagerTest.cs
+++ TeamGenerator.Infrastructure.Tests/PlayerDataManagerTest.cs
@@ -6,11 +6,18 @@ namespace TeamGenerator.Infrastructure.Tests
 {
     public class PlayerDataManagerTest
     {
+        string validTestPlayerPoolSerialization = 
+            "[{\"Nick\":\"John\",\"Rank\":{\"Name\":\"Master\",\"Value\":3}}," +
+            "{\"Nick\":\"Doe\",\"Rank\":{\"Name\":\"Blaster\",\"Value\":5}}," +
+            "{\"Nick\":\"Johnny\",\"Rank\":{\"Name\":\",.l;op[\",\"Value\":6}}," +
+            "{\"Nick\":\"Long\",\"Rank\":{\"Name\":\"1234567890\",\"Value\":9}}," +
+            "{\"Nick\":\"Silver\",\"Rank\":{\"Name\":\"1234asdf,.l;\",\"Value\":13}}]";
+
         List<Player> validTestPlayerPool = new List<Player>
         {
             new Player("John", new Rank("Master", 3)),
             new Player("Doe", new Rank("Blaster", 5)),
-            new Player("Johnny", new Rank(",./l;'op[", 6)),
+            new Player("Johnny", new Rank(",.l;op[", 6)),
             new Player("Long", new Rank("1234567890", 9)),
             new Player("Silver", new Rank("1234asdf,.l;", 13)),
         };
@@ -18,7 +25,10 @@ namespace TeamGenerator.Infrastructure.Tests
         [Test]
         public void PlayerPoolIsSerializedCorrectly()
         {
+            var playerDataManager = new PlayerDataManager();
+            string actualPlayerPoolSerialization = playerDataManager.SerializePlayerPool(validTestPlayerPool);
 
+            Assert.That(actualPlayerPoolSerialization == validTestPlayerPoolSerialization);
         }
     }
 }
--- TeamGenerator.Infrastructure/PlayerDataManager.cs
+++ TeamGenerator.Infrastructure/PlayerDataManager.cs
@@ -11,5 +11,11 @@ namespace TeamGenerator.Infrastructure
             string serializedPlayerPool = JsonSerializer.Serialize(playerPool);
             return serializedPlayerPool;
         }
+
+        public List<Player> DeserializePlayerPool(string serializedPlayerPool)
+        {
+            List<Player> playerPool = JsonSerializer.Deserialize<List<Player>>(serializedPlayerPool);
+            return playerPool;
+        }
     }
 }
