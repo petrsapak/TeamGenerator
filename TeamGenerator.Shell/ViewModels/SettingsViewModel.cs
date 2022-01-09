e7521c973b16539faafd49338fdffce9e4218298
Petr Sapak <petr.sapak@yahoo.com>
2022-01-09 12:39:27 +0100

    Move harcoded placeholders from XAML to C#

--- TeamGenerator.Shell/ViewModels/SettingsViewModel.cs
+++ TeamGenerator.Shell/ViewModels/SettingsViewModel.cs
@@ -2,5 +2,21 @@
 {
     internal class SettingsViewModel : ViewModelBase
     {
+        public SettingsViewModel()
+        {
+            Placeholder = "SETTINGS";
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get => placeholder;
+            set
+            {
+                placeholder = value;
+                RaisePropertyChanged(nameof(Placeholder));
+            }
+        }
     }
 }
--- TeamGenerator.Shell/ViewModels/StatisticsViewModel.cs
+++ TeamGenerator.Shell/ViewModels/StatisticsViewModel.cs
@@ -2,5 +2,21 @@
 {
     internal class StatisticsViewModel : ViewModelBase
     {
+        public StatisticsViewModel()
+        {
+            Placeholder = "STATISTICS";
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get => placeholder;
+            set
+            {
+                placeholder = value;
+                RaisePropertyChanged(nameof(Placeholder));
+            }
+        }
     }
 }
