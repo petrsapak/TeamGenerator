21c79f42792f5a4277f128bc55b4e89a99f879ed
Petr Sapak <petr.sapak@yahoo.com>
2022-01-09 12:14:12 +0100

    Remove hardcoded about message to viewModel

--- TeamGenerator.Shell/ViewModels/AboutViewModel.cs
+++ TeamGenerator.Shell/ViewModels/AboutViewModel.cs
@@ -2,5 +2,21 @@
 {
     internal class AboutViewModel : ViewModelBase
     {
+        public AboutViewModel()
+        {
+            AboutMessage = "https://github.com/petrsapak/TeamGenerator";
+        }
+
+        private string aboutMessage;
+
+        public string AboutMessage 
+        {
+            get => aboutMessage;
+            set
+            {
+                aboutMessage = value;
+                RaisePropertyChanged(nameof(AboutMessage));
+            }
+        }
     }
 }
