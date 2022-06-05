b0820e6fa6cc3e0e35b32807f4778245c4266609
Petr Sapak <petr.sapak@yahoo.com>
2022-06-05 10:00:52 +0200

    Update all statistics records

--- TeamGenerator.Model/Match.cs
+++ TeamGenerator.Model/Match.cs
@@ -1,28 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace TeamGenerator.Model
 {
     public class Match
     {
         public string CreationDate { get; set; }
-
-        [Obsolete("Use teams")]
-        public Team Team1 { get; set; }
-        [Obsolete("Use teams")]
-        public Team Team2 { get; set; }
-
-        [Obsolete("Use properties inside the team")]
-        public int Team1Score { get; set; }
-
-        [Obsolete("Use properties inside the team")]
-        public int Team2Score { get; set; }
-
-        [Obsolete("Use properties inside the team")]
-        public int Team1Probability { get; set; }
-        [Obsolete("Use properties inside the team")]
-        public int Team2Probability { get; set; }
-
         public IEnumerable<Team> Teams { get; set; }
 
         public Match()
